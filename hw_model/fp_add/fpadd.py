db0e81014dffa9deceddbb796ef2bd6d022d5d1c
jmg2027 <132035258+jmg2027@users.noreply.github.com>
2023-05-09 17:26:12 +0900

    Update fpadd.py
    algorithm adopted. need debug

--- hw_model/fp_add/fpadd.py
+++ hw_model/fp_add/fpadd.py
@@ -11,70 +11,83 @@ class FloatAddition:
         b_sign, b_exp, b_mant_nohidden = self.b.decompose_bf16()
         a_mant_us = bf16.ubit(self.a.mantissa_bits, f'1{a_mant_nohidden}')
         b_mant_us = bf16.ubit(self.b.mantissa_bits, f'1{b_mant_nohidden}')
-        a_mant_signed = bf16.sbit(self.a.mantissa_bits + 2, f'01{a_mant_nohidden}')
-        b_mant_signed = bf16.sbit(self.b.mantissa_bits + 2, f'01{b_mant_nohidden}')
 
         a_exp_signed = bf16.sbit(a_exp.bitwidth + 2, f'0{a_exp.bin}')
         b_exp_signed = bf16.sbit(a_exp.bitwidth + 2, f'0{b_exp.bin}')
         bias_signed = bf16.sbit(a_exp.bitwidth + 2, bin(self.a.bias))
 
         # if exponent difference is greater than mantissa bits + GRSSS (7 + 5), lesser number is considered as zero
-        sticky_bit_width = bf16.ubit(2, bin(3))
+#        sticky_bit_width = bf16.ubit(2, bin(3))
 
         # Special cases
         #input
         isnormal = False
-        # nan * ? -> nan
+        # nan + ? -> nan
         if self.a.isnan() or self.b.isnan():
             ret_exp_0 = bf16.bit(bf16.Bfloat16.exponent_bits, bin(bf16.Bfloat16.exp_max))
             ret_mant_0 = bf16.bit(bf16.Bfloat16.mantissa_bits, bin(bf16.Bfloat16.mant_max))
-        # inf * 0 = nan
+        # inf + 0 = nan ?
+        # tbd
         elif (self.a.isinf() and self.b.iszero()) or (self.a.iszero() and self.b.isinf()):
             ret_exp_0 = bf16.bit(bf16.Bfloat16.exponent_bits, bin(bf16.Bfloat16.exp_max))
             ret_mant_0 = bf16.bit(bf16.Bfloat16.mantissa_bits, bin(bf16.Bfloat16.mant_max))
-        # inf * !0 = inf
+        # inf + !0 = inf
         elif (self.a.isinf() and (not self.b.iszero())) or ((not self.a.iszero()) and self.b.isinf()):
             ret_exp_0 = bf16.bit(bf16.Bfloat16.exponent_bits, bin(bf16.Bfloat16.exp_max))
             ret_mant_0 = bf16.bit(bf16.Bfloat16.mantissa_bits, bf16.Bfloat16.mantissa_bits * '0')
-        # zero * x = zero
-        elif ((not self.a.isinf()) and self.b.iszero()) or (self.a.iszero() and (not self.b.isinf())):
-            ret_exp_0 = bf16.bit(bf16.Bfloat16.exponent_bits, bf16.Bfloat16.exponent_bits * '0')
-            ret_mant_0 = bf16.bit(bf16.Bfloat16.mantissa_bits, bf16.Bfloat16.mantissa_bits * '0')
+        # zero + x = x
+#        elif ((not self.a.isinf()) and self.b.iszero()) or (self.a.iszero() and (not self.b.isinf())):
+#            ret_exp_0 = bf16.bit(bf16.Bfloat16.exponent_bits, bf16.Bfloat16.exponent_bits * '0')
+#            ret_mant_0 = bf16.bit(bf16.Bfloat16.mantissa_bits, bf16.Bfloat16.mantissa_bits * '0')
         # normal case
         else:
             isnormal = True
 
         
         if isnormal:
+            # https://ieeexplore.ieee.org/stamp/stamp.jsp?arnumber=6489167
+            # Exponent calculation
             # Calculate Exponent differences
             exp_diff = a_exp_signed - b_exp_signed
             exp_diff_abs = abs(int(exp_diff))
-
             # Set flags
             a_exp_gt_b = exp_diff > bf16.sbit(1, '0')
             a_exp_eq_b = exp_diff == bf16.sbit(1, '0')
             a_mant_gt_b = a_mant_us > b_mant_us
             subtract_mant = a_sign ^ b_sign
-            # if exponent difference is greater than mantissa bits + GRSSS (7 + 5), lesser number is considered as zero?
-
+            # Prenormalized Exponent
             if a_exp_gt_b:
-                mant_unshift = a_mant_us
-                mant_shift_in = b_mant_us
+                ret_exp_0 = a_exp_signed
+            else:
+                ret_exp_0 = b_exp_signed
+
+            # Mantissa swap and shift
+            # a_mant_us = [7:0]
+            # mant_unshift = [7+3:0] -> grs
+            # mant_shift = [7+3:0] -> grs
+            # mant_shift_in = [7+2:0] -> grs, sticky bit will concat later
+            if a_exp_gt_b:
+                mant_unshift = a_mant_us.concat(bf16.ubit(3, '0'))
+                mant_shift_in = b_mant_us.concat(bf16.ubit(2, '0'))
             else:
                 mant_unshift = b_mant_us.concat(bf16.ubit(3, '0'))
-                mant_shift_in = a_mant_us.concat(bf16.ubit(2, '0'))
-
-            # Sticky bit
-            if exp_diff_abs >= bf16.Bfloat16.mantissa_bits +  2 + int(sticky_bit_width):
+                mant_shift_in = a_mant_us.concat(bf16.ubit(3, '0'))
+            # Sticky bit:
+            # if exp_diff is larger than mantissa bits + hidden bit + GRS then all shift mantissa is sticky
+            # else sticky is exp_diff - 3 ~ 0
+            if exp_diff_abs >= bf16.Bfloat16.mantissa_bits + 1 + 3:
                 mant_sticky = mant_shift_in.reduceor()
             else:
-                mant_sticky = mant_shift_in << bf16.Bfloat16.mantissa_bits +  2 + int(sticky_bit_width) - exp_diff_abs
+                mant_sticky = (mant_shift_in << (bf16.Bfloat16.mantissa_bits + 1 + 3 - exp_diff_abs)).reduceor()
             
             mant_shift = (mant_shift_in >> exp_diff_abs).concat(mant_sticky)
 
+            # Add/Sub Shifted mantissa /w LZA
+            # Invert flag: mantissa of lesser exponent for Sub
             if subtract_mant == bf16.bit(1, '1'):
                 if a_exp_gt_b:
-                    mant_inv_a = True
+                    mant_inv_b = True
+                # With equal exponent, invert lesser mantissa
                 elif a_exp_eq_b:
                     if a_mant_gt_b:
                         mant_inv_b = True
@@ -85,7 +98,10 @@ class FloatAddition:
             else:
                 mant_inv_a = False
                 mant_inv_b = False
-
+            
+            # Invert mantissa
+            # mant_unshift with grs= [7+3:0] 
+            # mant_shift with grs= [7+3:0]
             if mant_inv_a:
                 mant_add_in_a = ~mant_shift
                 mant_add_in_b = mant_unshift
@@ -95,11 +111,14 @@ class FloatAddition:
             else:
                 mant_add_in_a = mant_shift
                 mant_add_in_b = mant_unshift
-
+            
+            # Add mantissa (Including sub)
+            # mant_add[11:0] (Including carry)
             mant_add = mant_add_in_a + mant_add_in_b
-
             ret_mant_0 = mant_add
-
+            # Normalize with LZA shift amount when Sub
+            # apply lza later
+            # Sign calculation with Sub result
             # Sign Caculation
             if a_exp_gt_b:
                 ret_sign_0 = a_sign
@@ -110,39 +129,43 @@ class FloatAddition:
                     ret_sign_0 = b_sign
             else:
                 ret_sign_0 = b_sign
-
-            # Prenormalized Exponent
-            if a_exp_gt_b:
-                ret_exp_0 = a_exp_signed
+            # Exponent adjust with normalize/Add
+            # Sub: in case of 0x.xxxxxxx shift amount = lzc - 1
+            if subtract_mant:
+                exp_adj = bf16.hwutil.leading_zero_count(ret_mant_0) - 1
+            # Add, carry occured
+            elif ret_mant_0[ret_mant_0.bitwidth-1] == bf16.ubit(1, '1'):
+                exp_adj = 1
+            # Add, carry not occured
             else:
-                ret_exp_0 = b_exp_signed
+                exp_adj = 0
+                
+            ret_exp_1 = ret_exp_0 + bf16.sbit(ret_exp_0.bitwidth + 2, bin(exp_adj))
 
-            # Normalization
-            # Need right shift with lza
-            if ret_mant_0[ret_mant_0.bitwidth-1] == bf16.ubit(1, '1'):
-                ret_exp_1 = ret_exp_0 + bf16.ubit(ret_exp_0.bitwidth, '01')
-                ret_mant_1 = ret_mant_0
+            # Round and Postnormalization
+            if ret_mant_0 == bf16.ubit(12, '1111111111111111'):
+                # Postnormalize
+                ret_exp_2 = ret_exp_1 + bf16.sbit(ret_exp_0.bitwidth + 2, 1)
+                ret_mant_1 = bf16.ubit(8, '0')
             else:
-                ret_exp_1 = ret_exp_0
-                ret_mant_1 = ret_mant_0
-            # Round
-            # Need to check if ret_mant_2 if carry discards when rounds up 
-            # TBD: What's wrong with sticky bits??
-            ret_mant_2 = bf16.hwutil.round_to_nearest_even_bit(ret_mant_1, 8, 0)
-
-            # Post normalization
-            if ret_mant_2[ret_mant_2.bitwidth-1] == bf16.ubit(1, '1'):
-                ret_exp_2 = ret_exp_1 + bf16.ubit(ret_exp_1.bitwidth, '01')
-                ret_mant_3 = ret_mant_2[ret_mant_2.bitwidth-1:ret_mant_2.bitwidth-9]
-            else:
+                # round
                 ret_exp_2 = ret_exp_1
-                ret_mant_3 = ret_mant_2
+                ret_mant_1 = bf16.hwutil.round_to_nearest_even_bit(ret_mant_0, 8, 1)
             
-            ret_mant_4 = ret_mant_3[bf16.Bfloat16.mantissa_bits - 1:0]
+            # Check for overflow/underflow
+
+            # remove hidden bit
+            ret_mant_2 = ret_mant_1[bf16.Bfloat16.mantissa_bits - 1:0]
+        else:
+            ret_mant_2 = ret_mant_0
+            ret_exp_2 = ret_exp_0
             
         # Remove sign bit from exponent
         ret_exp_bit_2 = bf16.bit(a_exp.bitwidth, ret_exp_2.bin)
+        print(ret_mant_0)
+        print(ret_mant_1)
+        print(ret_mant_2)
 
         # Compose BF16
-        add = bf16.Bfloat16.compose_bf16(ret_sign_0, ret_exp_bit_2, ret_mant_4)
+        add = bf16.Bfloat16.compose_bf16(ret_sign_0, ret_exp_bit_2, ret_mant_2)
         return add
