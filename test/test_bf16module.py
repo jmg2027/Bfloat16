0c00dcb7ea7860ccebfa03475cb59caef242d190
jmg2027 <132035258+jmg2027@users.noreply.github.com>
2023-05-22 13:31:57 +0900

    Create test_bf16module.py

---
+++ test/test_bf16module.py
@@ -0,0 +1,39 @@
+from bf16.bf16 import Bfloat16 as bf16
+
+def test():
+    a = bf16(1,12,25)
+    #e = a.bf16_to_fp32()
+    #print(e)
+    #print(a.isnan())
+    #print(a)
+    #print(len(a.bin()))
+
+    b = bf16(0,32,17)
+    #f = b.bf16_to_fp32()
+    #print(f)
+    #print(b)
+
+    c = bf16.float_to_bf16(1.6)
+    d = bf16.float_to_bf16(-1.99999988079)
+
+    #g = c.bf16_to_fp32()
+    #print(g)
+    #
+    #print(c)
+    #print(d)
+    #
+    #print(c.bf16_to_float())
+    #print(d.bf16_to_float())
+    #
+    #print("Today")
+    #print(c.bf16_to_tfbf16())
+    #print(c.bf16_to_fp32())
+
+    print(a.decompose_bf16())
+    print(b.decompose_bf16())
+    print(c.decompose_bf16())
+    print(d.decompose_bf16())
+    return
+
+if __name__ == "__main__":
+    test()
