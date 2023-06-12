42ba4c54730b6c546c95829ffaf630dbd3dfe346
jmg2027 <132035258+jmg2027@users.noreply.github.com>
2023-06-12 17:23:26 +0900

    Update test2.py

--- test2.py
+++ test2.py
@@ -2,22 +2,38 @@ from bf16.bf16 import Bfloat16 as bf16
 import numpy as np
 import tensorflow as tf
 
-A = bf16.float_to_bf16(-30408704.0)
-B = bf16.float_to_bf16(3.844499588012695e-06)
-C = bf16.float_to_bf16(-0.310546875)
+#A = bf16.float_to_bf16(-30408704.0)
+#B = bf16.float_to_bf16(3.844499588012695e-06)
+#C = bf16.float_to_bf16(-0.310546875)
+#
+#p = A*B
+#
+#print(A)
+#print(B)
+#print(C)
+#
+#print(A.decompose_bf16())
+#print(B.decompose_bf16())
+#print(C.decompose_bf16())
+#
+#print(p)
+#print(p+C)
+#
+#print(p.decompose_bf16())
+#print((p+C).decompose_bf16())
 
-p = A*B
+A = bf16.float_to_bf16(-0.00173187255859375)
+B = bf16.float_to_bf16(744.0)
+C = bf16.float_to_bf16(-744.0)
+D = bf16.float_to_bf16(0.00811767578125)
+E = bf16.float_to_bf16(0.00640869)
 
 print(A)
 print(B)
 print(C)
+print(D)
 
-print(A.decompose_bf16())
-print(B.decompose_bf16())
-print(C.decompose_bf16())
-
-print(p)
-print(p+C)
-
-print(p.decompose_bf16())
-print((p+C).decompose_bf16())
+print(B+C)
+print(A+D)
+print(A+B+C+D)
+print(E)
