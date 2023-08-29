b020ee63848bfcfdb1d7fa2de851a4267d3ba88b
jmg2027 <132035258+jmg2027@users.noreply.github.com>
2023-08-29 17:55:30 +0900

    Update env.sh

--- script/env.sh
+++ script/env.sh
@@ -9,14 +9,18 @@ PS_NAME=$(basename $TOP_DIR)
 CUSTOM_PS1="($PS_NAME) $PS1"
 
 if [ ! -d $TOP_DIR/.venv ]; then
-    echo "Init .venv"
-    virtualenv -p /usr/bin/python3 $TOP_DIR/.venv
-    source $TOP_DIR/.venv/bin/activate
-    pip install -r $SCRIPT_DIR/requirements.txt
+    echo "no venv"
 else
-    source $TOP_DIR/.venv/bin/activate
+    rm -rf $TOP_DIR/.venv
 fi
+
+echo "Init .venv"
+python3.8 -m venv $TOP_DIR/.venv
+source $TOP_DIR/.venv/bin/activate
+pip install -r $SCRIPT_DIR/requirements.txt
+touch .done
+
 export PS1=$CUSTOM_PS1
 
+
 # Env variables
-export NPU_SIM_TOP_DIR=$TOP_DIR
