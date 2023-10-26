48a38cef7a4a173bf97c05a8811c0f17c4b731af
jmg2027 <132035258+jmg2027@users.noreply.github.com>
2023-10-26 17:37:03 +0900

    Update test_fma.py

--- test/test_fma.py
+++ test/test_fma.py
@@ -55,16 +55,15 @@ class TestFMA(TestOperationBase):
     # override
     def test_body(self, input: Tuple[Union[int, float, bf16, fp32], ...]) -> Tuple:
         # input: hex(int), float, bf16, fp32
-        operand_input = tuple(map(cast_float, input, [self.input_ftype]*(self._INPUT_NUM - 1)))
-        operand_output = map(cast_float, input, [self.ftype])
-        operand = operand_input + tuple(operand_output)
-        #print(operand)
+        operand_input = tuple(map(cast_float, (input[0], input[1]), [self.input_ftype]*(self._INPUT_NUM - 1)))
+        operand_output = cast_float(input[2], self.ftype)
+        operand = (*operand_input ,operand_output)
         res = self.operation(*operand)
         #print(res)
 
-        tf_operand_input = tuple(map(conv_to_tf_dtype, input, [self.ftype]*(self._INPUT_NUM - 1)))
-        tf_operand_output = map(conv_to_tf_dtype, input, [self.ftype])
-        tf_operand = tf_operand_input + tuple(tf_operand_output)
+        tf_operand_input = tuple(map(conv_to_tf_dtype, (input[0], input[1]), [self.input_ftype]*(self._INPUT_NUM - 1)))
+        tf_operand_output = conv_to_tf_dtype(input[2], self.ftype)
+        tf_operand = (*tf_operand_input, tf_operand_output)
         tfres = self.tf_operation(*tf_operand)
         
         if check_float_equal(res, tfres):
