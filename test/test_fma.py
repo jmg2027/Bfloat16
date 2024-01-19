ae1f2725f24d3131418db306749f36ec7695d317
jmg2027 <132035258+jmg2027@users.noreply.github.com>
2024-01-19 19:42:57 +0900

    Update test_fma.py

--- test/test_fma.py
+++ test/test_fma.py
@@ -26,7 +26,7 @@ class TestFMA(TestOperationBase):
     #        (-1.0, -2.0, -3.0),
     #        (-100.0, 2.0, 3.0),
     #        (100.0, 2.0, -3.0),
-            (10000.0, -2.0, 3.0),
+    #        (10000.0, -2.0, 3.0),
             # Special cases
     #        (0, 2.8, 4.5),
     #        (0, 0, 4.5),
@@ -43,7 +43,21 @@ class TestFMA(TestOperationBase):
     # FAILED fma(Float32(-3.4955471162181546e-23, sign = 1, exponent=-75, mantissa=2689223), Float32(3.922678904061092e+20, sign = 0, exponent=68, mantissa=2760316), Float32(0.01334461197257042, sign = 0, exponent=-7, mantissa=5940060)), lib: Float32(-0.0003672972379717976, sign = 1, exponent=-12, mantissa=4231629), tf: -0.000367296946933493, ulp_error: 10
     #(float('nan'), 1.0, 2.0),
 
-    #(bf16_obj.from_hex(0x08f1), bf16_obj.from_hex(0xffad), bf16_obj.from_hex(0x774b))
+    #(bf16_obj.from_hex(0x08f1), bf16_obj.from_hex(0xffad), bf16_obj.from_hex(0x774b)),
+#    (fp32(sign = 0, exponent=-36, mantissa=6579274), fp32(sign = 0, exponent=102, mantissa=6455005), fp32(sign = 0, exponent=-37, mantissa=7291628)),
+#    (fp32(sign = 1, exponent=110, mantissa=4879951), fp32(sign = 0, exponent=-22, mantissa=6206616), fp32(sign = 1, exponent=79, mantissa=7117663)),
+    # almost zero case
+#    (fp32(sign = 0, exponent=-16, mantissa=3610392), fp32(sign = 0, exponent=-111, mantissa=7339142), fp32(sign = 0, exponent=-126, mantissa=146679)),
+#    (fp32(sign = 0, exponent=-6, mantissa=1669669), fp32(sign = 0, exponent=-120, mantissa=489366), fp32(sign = 1, exponent=-125, mantissa=870642)),
+#    (fp32(sign = 0, exponent=-1, mantissa=5494362), fp32(sign = 1, exponent=-126, mantissa=3357491), fp32(sign = 0, exponent=-126, mantissa=5231156)),
+#    (fp32(sign = 1, exponent=122, mantissa=2374758), fp32(sign = 0, exponent=4, mantissa=6939185), fp32(sign = 1, exponent=127, mantissa=5624403)),
+#    (fp32(sign = 0, exponent=-82, mantissa=116089), fp32(sign = 0, exponent=-44, mantissa=5087916), fp32(sign = 1, exponent=-126, mantissa=5820685)),
+    #(fp32(sign = 1, exponent=62, mantissa=7427298), fp32(sign = 1, exponent=65, mantissa=5220737), fp32(sign = 1, exponent=127, mantissa=5526448)),
+    #(fp32(sign = 1, exponent=-121, mantissa=5557884), fp32(sign = 0, exponent=42, mantissa=407632), fp32(sign = 0, exponent=-79, mantissa=6235593)),
+    #(fp32(sign = 0, exponent=-61, mantissa=2616410), fp32(sign = 1, exponent=109, mantissa=1622550), fp32(sign = 1, exponent=24, mantissa=4674590)), 
+#    (fp32(sign = 0, exponent=84, mantissa=7180711), fp32(sign = 1, exponent=-62, mantissa=5976674), fp32(sign = 1, exponent=-2, mantissa=780509)),
+#    (fp32(sign = 1, exponent=48, mantissa=3825956), fp32(sign = 1, exponent=-13, mantissa=3249702), fp32(sign = 0, exponent=10, mantissa=533179)),
+    (fp32(sign = 1, exponent=-83, mantissa=32340), fp32(sign = 1, exponent=3, mantissa=93621), fp32(sign = 1, exponent=-104, mantissa=7984477)),
     ]
  
     test_operation = 'fma'
@@ -71,7 +85,7 @@ class TestFMA(TestOperationBase):
         operand_input = tuple(map(cast_float, (input[0], input[1]), [self.input_ftype]*(self._INPUT_NUM - 1)))
         operand_output = cast_float(input[2], self.ftype)
         operand = (*operand_input ,operand_output)
-        res = self.operation(*operand)
+        res = self.operation(*operand, algorithm = "MULTI_PATH")
         #print(res)
 
         ## when input operands are bf16 and output is fp32, tf dtype conversion should be like this:
@@ -88,6 +102,7 @@ class TestFMA(TestOperationBase):
         # for precision, cast operands to tf.float64. and cast result to ftype
         tfres = self.tf_operation(*tf_operand)
         tfres = conv_from_tf_to_tf_dtype(tfres, self.ftype)
+        tfres = check_subnorm(tfres)
         
         #test_res_str = f'{[i.hex() for i in (*operand_input, operand_output)]}\t\t{res.hex()}'
         if check_float_equal(res, tfres):
@@ -106,13 +121,29 @@ class TestFMA(TestOperationBase):
     def rand_test(self, times: int):
         test_list = []
         fail_list = []
+        
         for i in range(times):
+            # JMG: fix here
+            #exp_a, exp_b, exp_c = self.set_exponent(24)
+            #exp_a, exp_b, exp_c = self.set_exponent(24)
+            #exp_a, exp_b, exp_c = self.set_exponent(10)
+            #exp_a, exp_b, exp_c = self.set_exponent(-10)
+            #(a, b, c), fp32_res, test_res_str = self.test_body((random_fp(self.input_ftype, exp_a, exp_a), random_fp(self.input_ftype, exp_b, exp_b), random_fp(self.ftype, exp_c, exp_c)))
             (a, b, c), fp32_res, test_res_str = self.test_body((random_fp(self.input_ftype), random_fp(self.input_ftype), random_fp(self.ftype)))
             test_list.append([a, b, c, fp32_res])
             if check_fail_status(test_res_str):
                 fail_list.append(test_res_str)
         check_fail_list(fail_list)
         return test_list
+
+    def set_exponent(self, i: int):
+        #diff = random.randint(i, bf16_config.exp_max)
+        diff = i
+        exp_a = random.randint(-bf16_config.exp_max + 1, bf16_config.exp_max)
+        exp_b = random.randint(-bf16_config.exp_max + 1, bf16_config.exp_max)
+        exp_c_pre = exp_a + exp_b - diff
+        exp_c = exp_c_pre if (-bf16_config.exp_max + 1 < exp_c_pre) and (exp_c_pre < bf16_config.exp_max) else 0
+        return exp_a, exp_b, exp_c
 
     # override
     def test(self):
